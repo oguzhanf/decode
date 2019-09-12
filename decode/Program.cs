b5f992526cb0f0ce5da162a2f4aa76529d6ad3c9
oguzhanf <filizlibay@hotmail.com>
2019-09-12 18:31:22 +0400

    initial..

---
+++ decode/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace decode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+                usage();
+        }
+
+        static void usage()
+        {
+            Console.WriteLine("Usage: ");
+
+        }
+    }
+}
